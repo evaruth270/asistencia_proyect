180691311649e062bb170ff20298c809ea514fe7
eva <evaruthmamani21@gmal.com>
2024-09-25 18:33:45 -0700

    conexion ala base de datos

---
+++ gestion_personas.py
@@ -0,0 +1,27 @@
+import requests
+import sqlite3
+import tkinter as tk
+from tkinter import messagebox, ttk, filedialog
+from datetime import datetime, timedelta
+import pandas as pd
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+# Función para crear la base de datos y la tabla personas
+def crear_base_de_datos():
+    conexion = sqlite3.connect('nueva_base_de_datos.db')
+    cursor = conexion.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS personas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            apellido_paterno TEXT,
+            apellido_materno TEXT,
+            dni TEXT UNIQUE,
+            lugar_procedencia TEXT,
+            fecha TEXT,
+            hora TEXT
+        )
+    ''')
+    conexion.commit()
+    conexion.close()
