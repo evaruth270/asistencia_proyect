518bcde99af993a98ebbaa107434fffb0957f148
Yorchisflrs <140905225+Yorchisflrs@users.noreply.github.com>
2024-09-29 15:18:31 -0500

    holanena

--- database_manager.py
+++ database_manager.py
@@ -1,1 +1,21 @@
 #git clone https://github.com/evaruth270/asistencia_proyect.git
+
+# Función para crear la base de datos y la tabla personas
+def crear_base_de_datos():
+    conexion = sqlite3.connect('nueva_base_de_datos.db')
+    cursor = conexion.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS personas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            apellido_paterno TEXT,
+            apellido_materno TEXT,
+            dni TEXT UNIQUE,
+            lugar_procedencia TEXT,
+            fecha TEXT,
+            hora TEXT
+        )
+    ''')
+    conexion.commit()
+    conexion.close()
+#print("hola nena")
